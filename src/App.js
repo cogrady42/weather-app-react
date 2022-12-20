84903b4c37e850f9861017a664d9ff91008524bc
cogrady42 <ceciliaogrady42@gmail.com>
2022-12-20 14:25:37 -0500

    added github repo url

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ export default function App() {
   <h1>
     Hello!
   </h1>
-
+  <footer>
+    This project was coded by Cecilia O'Grady and is{" "}
+  <a href='https://github.com/cogrady42/weather-app-react' target="_blank">Open-sourced on Github</a>
+  </footer>
     </div>
   );
 }
