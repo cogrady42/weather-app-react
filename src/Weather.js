68cdf56e17ab506627ecd10428c4c8fc3e30dda8
cogrady42 <ceciliaogrady42@gmail.com>
2022-12-20 17:01:21 -0500

    added API call

--- src/Weather.js
+++ src/Weather.js
@@ -1,31 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-        let weatherData = {
-          date: "wednesday 10:21pm",
-          city: "Toronto",
-          temperature: 19,
-          humidity: 69,
-          precipitation: 50,
-          wind: 10,
-          feelslike: 25,
-          description: "Cloudy"
-        };
-      
+export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState({ ready:false });
+
+    function handleResponse(response) {
+        console.log(response.data.main)
+
+        setWeatherData({
+            ready: true,
+            humidity: response.data.main.humidity,
+            wind: response.data.wind.speed,
+            temperature: response.data.main.temp,
+            feelsLike: response.data.main.feels_like,
+            description: response.data.weather[0].description,
+        });
+    }
+
+      if (weatherData.ready) { 
         return (
           <div className="Weather">
             <div className="container-fluid">
               <div className="weather-app-wrapper">
                 <div className="weather-app">
                   <div className="row">
-                    <div className="col date">{weatherData.date}</div>
+                    <div className="col date">tuesday 4:56pm</div>
                     <div className="col">
                       <form className="change-city">
                         <input
                           type="search"
                           placeholder="change city"
-                          autocomplete="off"
+                          autoComplete="off"
                           className="change-city"
                           autoFocus="on"
                         />
@@ -38,26 +44,25 @@ export default function Weather() {
                     </div>
                   </div>
       
-                  <h1>{weatherData.city}</h1>
+                  <h1>Toronto</h1>
       
                   <div className="row align-items-center">
                     <div className="col">
                       <ul className="details">
                         <li>humidity: {weatherData.humidity}% </li>
-                        <li>wind: {weatherData.wind}km/h </li>
-                        <li>precipitation: {weatherData.precipitation}% </li>
+                        <li>wind: {Math.round(weatherData.wind)}km/h </li>
                       </ul>
                     </div>
                     <div className="col">
                       <div className="temperature">
-                        <span> {weatherData.temperature}</span>
+                        <span> {Math.round(weatherData.temperature)}</span>
                         <span className="unit">°C</span>
                       </div>
                     </div>
                     <div className="col">
                       <div className="feels-like">
                         feels like
-                        <div className="temperature2">{weatherData.feelslike}°</div>
+                        <div className="temperature2">{Math.round(weatherData.feelsLike)}°</div>
                       </div>
                     </div>
                   </div>
@@ -69,12 +74,21 @@ export default function Weather() {
                  Open-source Code  
               </a>
               <span>
-                  {""} by Cecilia O'Grady
+                  {" "} by Cecilia O'Grady
               </span>
             </div>
               </div>
            </div>
            </div>
         );
+      } else {
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`
+    axios.get(apiUrl).then(handleResponse);
+   
+
+    return "Loading..."
+
+      } 
 }
        
